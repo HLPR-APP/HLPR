6eff218568741f689381c65b6e961568765e37e5
Frank Hollywood <github@srvsync.com>
2022-01-17 11:01:21 -0800

    fixed private router, userprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext.jsx';
 import AboutUs from './views/AboutUs/AboutUs.jsx';
 import Login from './views/Login/Login.jsx';
 import OfferDetail from './views/OfferDetail/OfferDetail.jsx';
 import Profile from './views/Profile/Profile.jsx';
 import TaskDetail from './views/TaskDetail/TaskDetail.jsx';
 import TaskList from './views/TaskList/TaskList.jsx';
-//import { PrivateRoute } from './components/PrivateRoute/PrivateRoute';
-//import { ProvideAuth } from './context/ProvideAuth';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Login />
-      </Route>
-      <Route exact path="/tasklist">
-        <TaskList />
-      </Route>
-      <Route exact path="/profile">
-        <Profile />
-      </Route>
-      <Route exact path="/taskdetail/:id">
-        <TaskDetail />
-      </Route>
-      <Route exact path="/offerdetail/:id">
-        <OfferDetail />
-      </Route>
-      <Route exact path="/aboutus">
-        <AboutUs />
-      </Route>
-    </Switch>
+    <UserProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/tasklist">
+            <TaskList />
+          </Route>
+          <PrivateRoute exact path="/profile">
+            <Profile />
+          </PrivateRoute>
+          <Route exact path="/taskdetail/:id">
+            <TaskDetail />
+          </Route>
+          <Route exact path="/offerdetail/:id">
+            <OfferDetail />
+          </Route>
+          <Route exact path="/aboutus">
+            <AboutUs />
+          </Route>
+        </Switch>
+      </Router>
+    </UserProvider>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 render(
   <React.StrictMode>
-    <Router>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Router>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
