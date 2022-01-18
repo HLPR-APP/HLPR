40f014788c81cde2f8d0cf235b337ba3a20bf33a
Frank Hollywood <github@srvsync.com>
2022-01-18 12:46:14 -0800

    fixed login/signup logout

--- src/views/Login/Signup.jsx
+++ src/views/Login/Signup.jsx
@@ -1,50 +1,50 @@
-import Header from '../../components/Header/Header.jsx';
 import { useState } from 'react';
 import { useUser } from '../../context/UserContext.jsx';
 import { signUpUser } from '../../services/users.js';
+import { Button, FormControl, Input, FormLabel } from '@chakra-ui/react';
 
 export default function Signup() {
   const { setUser } = useUser();
   const [email, setEmail] = useState('');
-  const [pw, setPW] = useState('')
+  const [pw, setPW] = useState('');
   const [input, setInput] = useState('');
 
-
   const handleSubmit = async (e) => {
-    try{
-    e.preventDefault();
-    const userIn = await signUpUser(email, pw);
-    console.log(userIn);
-    console.log('hello')
-    
-    } catch (error){
-
-  }
-}
+    try {
+      e.preventDefault();
+      const userIn = await signUpUser(email, pw);
+      console.log(userIn);
+      console.log('hello');
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <>
-      <Header />
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='email'>Email
-            <input 
-        
-          id="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        </label>
-        <label htmlFor='password'>Password
-            <input 
-          id="password"
-          type="password"
-          value={pw}
-          onChange={(e) => setPW(e.target.value)}
-        />
-        </label>
-        <button type='submit'>Submit</button>
-        </form>
-
-    </>
+    <form onSubmit={handleSubmit}>
+      <FormControl>
+        <FormLabel htmlFor="email">
+          Email
+          <Input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormLabel>
+        <FormLabel htmlFor="password">
+          Password
+          <Input
+            id="password"
+            type="password"
+            value={pw}
+            onChange={(e) => setPW(e.target.value)}
+          />
+        </FormLabel>
+        <Button type="submit">Submit</Button>
+        <p>
+          Have an account?<a href="/">Sign in</a>
+        </p>
+      </FormControl>
+    </form>
   );
 }
