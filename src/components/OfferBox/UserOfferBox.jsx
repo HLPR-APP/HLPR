ed3679b5e9fafba13c6a01bca659c6dd4ff9b5ed
Estefani <stef45689@gmail.com>
2022-01-19 15:07:13 -0600

    add offer functionality

--- src/components/OfferBox/UserOfferBox.jsx
+++ src/components/OfferBox/UserOfferBox.jsx
@@ -2,14 +2,13 @@ import { Box, Badge, Image, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { deleteOfferByID } from '../../services/services';
 
-export default function UserOfferBox({offer}) {
-  
+export default function UserOfferBox({ offer }) {
   const removeOffer = async (offerID) => {
     const response = await deleteOfferByID(offerID);
     console.log(response);
     window.location.reload();
-  }
-  
+  };
+
   return (
     <>
       <Box
@@ -19,15 +18,15 @@ export default function UserOfferBox({offer}) {
         borderRadius="lg"
         overflow="hidden"
         fit="cover"
-      ><Badge borderRadius="full" px="2" colorScheme="teal" fontSize='md'>
-        
-       {offer.Tasks2.name}
-      </Badge>
+      >
+        <Badge borderRadius="full" px="2" colorScheme="teal" fontSize="md">
+          {offer.Tasks2.name}
+        </Badge>
         <Box p="6">
           <Box display="flex" alignItems="baseline">
             <Badge borderRadius="full" px="2" colorScheme="teal">
-            OfferedBy:{<br/>}
-            {offer.offered_by}
+              OfferedBy:{<br />}
+              {offer.offered_by}
             </Badge>
             <Box
               color="gray.500"
@@ -39,29 +38,37 @@ export default function UserOfferBox({offer}) {
             >
               Price: {offer.price}
             </Box>
-            {offer.accepted ? <Box
-              color="teal.500"
-              fontWeight="bold"
-              letterSpacing="wide"
-              fontSize="xs"
-              textTransform="uppercase"
-              ml="2"
-            >
-              Status: Offer Accepted
-            </Box> :
-            <Box
-            color="teal.500"
-            fontWeight="bold"
-            letterSpacing="wide"
-            fontSize="sm"
-            textTransform="uppercase"
-            ml="2"
-          >
-            Status: Not Accepted
-          </Box>}
+            {offer.accepted ? (
+              <Box
+                color="teal.500"
+                fontWeight="bold"
+                letterSpacing="wide"
+                fontSize="xs"
+                textTransform="uppercase"
+                ml="2"
+              >
+                Status: Offer Accepted
+              </Box>
+            ) : (
+              <Box
+                color="teal.500"
+                fontWeight="bold"
+                letterSpacing="wide"
+                fontSize="sm"
+                textTransform="uppercase"
+                ml="2"
+              >
+                Status: Not Accepted
+              </Box>
+            )}
           </Box>
-          <Button onClick={()=>removeOffer(offer.id)} size="sm" margin="2" colorScheme="blue">
-              Remove Offer
+          <Button
+            onClick={() => removeOffer(offer.id)}
+            size="sm"
+            margin="2"
+            colorScheme="blue"
+          >
+            Remove Offer
           </Button>
           <Box>
             <Box as="span" color="gray.600" fontSize="sm"></Box>
@@ -71,4 +78,3 @@ export default function UserOfferBox({offer}) {
     </>
   );
 }
-       
