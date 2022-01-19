2f9fc37ef8093e307f05c6532bf45fc76ba5c7cc
AmitSheth9 <amit.sheth9@gmail.com>
2022-01-19 01:21:53 -0600

    add task form working, offers made showing, remove offer working

--- src/components/AddTaskForm/AddTaskForm.jsx
+++ src/components/AddTaskForm/AddTaskForm.jsx
@@ -1,14 +1,44 @@
-import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { useState } from 'react';
+import { useUser } from '../../context/UserContext';
+import { AddTask } from '../../services/services';
 
 export default function AddTaskForm() {
+  const auth = useUser();
+  const [taskname, setTaskName] = useState('');
+  const [description, setDescription] = useState('');
+  const [imageURL, setImageURL] = useState('');
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const response = await AddTask(auth.user.email, taskname, description, imageURL)
+    console.log(response);
+    window.location.reload();
+  }
+
   return (
+
+    <form onSubmit={handleSubmit}>
     <FormControl m="10">
       <FormLabel htmlFor="taskname">Task Name</FormLabel>
-      <Input id="taskname" type="taskname" />
+      <Input 
+        id="taskname"
+        value={taskname}
+        onChange={(e)=> setTaskName(e.target.value)} />
       <FormLabel htmlFor="description">Description</FormLabel>
-      <Input id="description" type="description" />
-      <FormLabel htmlFor="date">Date</FormLabel>
-      <Input id="date" type="date" />
+      <Input 
+      id="description"
+      value={description}
+      onChange={(e) => setDescription(e.target.value)} 
+      />
+      <FormLabel htmlFor="date">Image-URL</FormLabel>
+      <Input
+      id='imageURL'
+      value={imageURL}
+      onChange={(e)=> setImageURL(e.target.value)} 
+      />
     </FormControl>
+    <Button type='submit'>Submit Task</Button>
+    </form>
   );
 }
--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -6,38 +6,83 @@ import { useUser } from '../../context/UserContext.jsx';
 import Tasks from '../../components/Tasks/Tasks.jsx';
 import { getOffersByTaskEmail } from '../../services/services.js';
 import styles from './Profile.css';
+import { getOffersByUser } from '../../services/services.js';
+import { updateAcceptTask } from '../../services/services.js';
+import { updateOfferAccepted } from '../../services/services.js';
+import ProfileTaskBox from '../../components/Profile/ProfileTaskBox.jsx';
+import { deleteOfferByID } from '../../services/services.js';
 
 export default function Profile() {
   const auth = useUser();
   const [userTasks, setUserTasks] = useState([]);
   const [offers, setOffers] = useState([]);
+  const [offered, setOffered] = useState([]);
+  
   useEffect(() => {
     const onMount = async () => {
       const data = await getTasksByEmail(auth.user.email);
-      console.log(data);
       setUserTasks(data);
       const returnedOffers = await getOffersByTaskEmail(auth.user.email);
       console.log('offers', returnedOffers);
       setOffers(returnedOffers);
+      const userOffers = await getOffersByUser(auth.user.email)
+      setOffered(userOffers);
+      console.log(userOffers);
     };
     onMount();
   }, []);
+
+  const handleAcceptOffer = async (taskID, offerID) => {
+    try{
+    //console.log(taskID);
+    //const response = await updateAcceptTask(taskID);
+    //console.log(response);
+    const returned = await updateOfferAccepted(offerID);
+   console.log(returned);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+  const removeOffer = async (offerID) => {
+    const response = await deleteOfferByID(offerID);
+    console.log(response);
+    window.location.reload();
+  }
+
   console.log(userTasks);
 
   return (
     <>
-      <AddTaskForm />
-      {/* hide form */}
+      <AddTaskForm /><br/><br/>
+
       Your Offers:
       {offers.map((offer) => (
         <div className={styles.offer_container} key={offer.id}>
-          <p className={styles.offer_detail}>{offer.Tasks2.name}</p>
+          <p className={styles.offer_task}>{offer.Tasks2.name}</p>
           <p className={styles.offer_detail}>Offered By: {offer.offered_by}</p>
           <p className={styles.offer_detail}>${offer.price}</p>
           <p className={styles.offer_detail}>Offer Date: {offer.date}</p>
+          <button onClick={() => handleAcceptOffer(offer.Tasks2.id, offer.id)} className={styles.accept}>Accept Offer</button>
         </div>
+      ))}<br/>
+      Your Tasks...
+      
+      {userTasks.map((task) => (
+        <ProfileTaskBox key={task.id} task={task} />
       ))}
-      <Tasks tasks={userTasks} />
+
+      <div>
+        <div>Offers you have made...</div>
+        {offered.map((offer) => (
+          <div className={styles.offer_container} key={offer.id}>
+            <p className={styles.offer_task}> {offer.Tasks2.name}</p>
+            <p className={styles.offer_detail}>Offer Price: {offer.price}</p>
+            <p className={styles.offer_detail}>Offered By You:  {offer.offered_by}</p>
+            {offer.accepted ? <p className={styles.offer_task}>Status: Accepted {offer.Tasks.posted_by}</p> : <p className={styles.offer_task}>Status: Not accepted</p>}
+            <button className={styles.accept} onClick={()=>removeOffer(offer.id)}>Remove Offer</button>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
