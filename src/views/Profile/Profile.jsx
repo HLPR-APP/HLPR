5a9f6e03004187148427104fcce649f6767ea35e
AmitSheth9 <amit.sheth9@gmail.com>
2022-01-18 19:00:24 -0600

    profile page basics

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,23 +1,45 @@
 import { useState, useEffect } from 'react';
 import AddTaskForm from '../../components/AddTaskForm/AddTaskForm.jsx';
 import { getTasksByUser } from '../../services/services.js';
+import { getTasksByEmail } from '../../services/services.js';
+import { useUser } from '../../context/UserContext.jsx';
+import Tasks from '../../components/Tasks/Tasks.jsx';
+import { getOffersByTaskEmail } from '../../services/services.js';
+import styles from './Profile.css'
 
 export default function Profile() {
+  const auth = useUser();
+  console.log(auth.user.email);
   const [userTasks, setUserTasks] = useState([]);
+  const [offers, setOffers] = useState([]);
   useEffect(() => {
     const onMount = async () => {
-      const data = await getTasksByUser('c1877939-d12d-45b0-92bc-a9e5b475c684');
+      const data = await getTasksByEmail(auth.user.email);
+      console.log(data);
       setUserTasks(data);
+      const returnedOffers = await getOffersByTaskEmail(auth.user.email);
+      console.log('offers', returnedOffers);
+      setOffers(returnedOffers);
     };
-  }, []);
+  onMount()}, []);
   console.log(userTasks);
+  
   return (
     <>
+    <AddTaskForm />
       {/* hide form */}
-      <AddTaskForm />
-      {userTasks.map((task) => (
-        <div key={task.task_id}>{task.name}</div>
+      Your Offers:{offers.map((offer) => (
+        <div className={styles.offer_container} key={offer.id}>
+          <p className={styles.offer_detail}>{offer.Tasks2.name}</p>
+          <p className={styles.offer_detail}>Offered By: {offer.offered_by}</p>
+          <p className={styles.offer_detail}>${offer.price}</p>
+          <p className={styles.offer_detail}>Offer Date: {offer.date}</p>
+        </div>
       ))}
-    </>
+      
+      <Tasks tasks={userTasks} />
+      
+     
+     </>
   );
 }
